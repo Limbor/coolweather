d1aa409eb7c17b16ca71567391f479629c52ee7e
Limbor <18721912858@126.com>
2018-12-11 23:32:22 +0800

    增加后台更新天气和背景图的功能

--- app/src/main/java/com/project/asus/coolweather/WeatherActivity.java
+++ app/src/main/java/com/project/asus/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.project.asus.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.project.asus.coolweather.gson.Forecast;
 import com.project.asus.coolweather.gson.Weather;
+import com.project.asus.coolweather.service.AutoUpdateService;
 import com.project.asus.coolweather.util.HttpUtil;
 import com.project.asus.coolweather.util.Utility;
 
@@ -181,6 +183,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 
     //获得必应图片
